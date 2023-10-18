984707dbac1af478c0129052884e4db41b074be5
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-10-18 15:17:15 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,69 +4,45 @@ import { useState } from "react";
 const App = () => {
   const [seekTime, handleSeekTime] = useState<number>(5);
 
-  const handlePause = async () => {
+  const handlePlayback = async (type: string) => {
     let [tab] = await chrome.tabs.query({ active: true });
     chrome.scripting.executeScript({
       target: { tabId: tab.id! },
       world: "MAIN",
-      func: () => {
+      func: (type) => {
         const videoElem = document.querySelector("video");
 
         if (videoElem && videoElem instanceof HTMLVideoElement) {
-          if (!videoElem.paused) {
+          if (!videoElem.paused && type === "pause") {
             videoElem.pause();
-          }
-        }
-      },
-    });
-  };
-
-  const handlePlay = async () => {
-    let [tab] = await chrome.tabs.query({ active: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id! },
-      world: "MAIN",
-      func: () => {
-        const videoElem = document.querySelector("video");
-
-        if (videoElem && videoElem instanceof HTMLVideoElement) {
-          if (videoElem.paused) {
+          } else {
             videoElem.play();
           }
         }
       },
+      args: [type],
     });
   };
 
-  const handleForward = async () => {
+  const handleSeek = async (type: string) => {
     let [tab] = await chrome.tabs.query({ active: true });
     chrome.scripting.executeScript({
       target: { tabId: tab.id! },
       world: "MAIN",
-      func: () => {
+      func: (seekTime: number, type: string) => {
         const videoElem = document.querySelector("video");
 
         if (videoElem && videoElem instanceof HTMLVideoElement) {
-          videoElem.currentTime += seekTime;
+          if (type === "backward") {
+            videoElem.currentTime -= seekTime;
+          } else {
+            videoElem.currentTime += seekTime;
+          }
         }
       },
+      args: [seekTime, type],
     });
-  }
-
-  const handleBackward = async () => {
-    let [tab] = await chrome.tabs.query({ active: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id! },
-      world: "MAIN",
-      func: () => {
-        const videoElem = document.querySelector("video");
-
-        if (videoElem && videoElem instanceof HTMLVideoElement) {
-          videoElem.currentTime -= seekTime;
-        }
-      },
-    });
-  }
+  };
 
   return (
     <div className="w-[20rem] h-[25rem] bg-black p-10">
@@ -74,17 +50,37 @@ const App = () => {
         Enhanced Youtube Experience
       </h1>
       <div className=" w-fit mx-auto mt-10 flex flex-col gap-5">
-        <button className="bg-white px-5 py-2" onClick={handlePlay}>
+        <button
+          className="bg-white px-5 py-2"
+          onClick={() => {
+            handlePlayback("play");
+          }}
+        >
           Play
         </button>
-        <button className="bg-white px-5 py-2" onClick={handlePause}>
+        <button
+          className="bg-white px-5 py-2"
+          onClick={() => {
+            handlePlayback("pause");
+          }}
+        >
           Pause
         </button>
-        <div className="flex gap-2">
-          <button className="px-5 py-2 bg-white" onClick={handleBackward}>
+        <div className="flex gap-2 justify-center w-full">
+          <button
+            className="px-5 py-2 bg-white w-full"
+            onClick={() => {
+              handleSeek("backward");
+            }}
+          >
             <BsFastForwardFill className="scale-x-[-1]" />
           </button>
-          <button className="px-5 py-2 bg-white" onClick={handleForward}>
+          <button
+            className="px-5 py-2 bg-white w-full"
+            onClick={() => {
+              handleSeek("forward");
+            }}
+          >
             <BsFastForwardFill />
           </button>
         </div>
@@ -97,7 +93,7 @@ const App = () => {
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               const value = parseInt(e.target.value);
 
-              handleSeekTime(value)
+              handleSeekTime(value);
             }}
           />
           <h2 className="text-white text-center">Seek Time: {seekTime}</h2>
